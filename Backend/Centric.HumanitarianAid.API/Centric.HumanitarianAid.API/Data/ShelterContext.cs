f9cfa4314adcfcba57e1d24d8e3bb9d2e20a6bda
Cristiana Grigoriu <cristiana.grigoriu@gmail.com>
2022-03-14 21:16:13 +0200

    Add nuget packages and class for db connection

---
+++ Backend/Centric.HumanitarianAid.API/Centric.HumanitarianAid.API/Data/ShelterContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Centric.HumanitarianAid.API.Data
+{
+    using Business;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ShelterContext : DbContext
+    {
+        public DbSet<Shelter> Shelters { get; set; }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
