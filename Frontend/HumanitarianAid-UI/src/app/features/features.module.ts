0530a42fdd8bf51d0e5c86402b5d9d701973552c
Barzu Gabriel <barzu.gabriel@yahoo.com>
2022-03-24 16:51:34 +0200

    Add title and filters

--- Frontend/HumanitarianAid-UI/src/app/features/features.module.ts
+++ Frontend/HumanitarianAid-UI/src/app/features/features.module.ts
@@ -5,9 +5,21 @@ import { SheltersComponent } from './shelters/shelters.component';
 import { PersonsComponent } from './persons/persons.component';
 import { ContactComponent } from './contact/contact.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+const materialImports = [
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatSlideToggleModule,
+];
+
 @NgModule({
   declarations: [SheltersComponent, PersonsComponent, ContactComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, materialImports],
   exports: [SheltersComponent, PersonsComponent, ContactComponent],
 })
 export class FeaturesModule {}
