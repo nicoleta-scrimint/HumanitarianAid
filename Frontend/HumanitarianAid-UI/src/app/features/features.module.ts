9b49b7d9cea4ba919d49d7bdc2bcd88353b0a0c0
Sergiu Andrei <sergiu.boistean@centric.eu>
2022-03-30 11:30:12 +0300

    Created page

--- Frontend/HumanitarianAid-UI/src/app/features/features.module.ts
+++ Frontend/HumanitarianAid-UI/src/app/features/features.module.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 
@@ -12,12 +15,15 @@ import { SheltersComponent } from './shelters/shelters.component';
 
 const materialImports = [
   MatTableModule,
-  MatSnackBarModule
+  MatSnackBarModule,
+  MatPaginatorModule,
+  MatSelectModule,
+  MatInputModule
 ]
 
 @NgModule({
   declarations: [SheltersComponent, PersonsComponent, ContactComponent, ShelterCardComponent],
-  imports: [CommonModule, HttpClientModule, materialImports, ],
+  imports: [CommonModule, HttpClientModule, materialImports],
   exports: [SheltersComponent, PersonsComponent, ContactComponent],
   providers: [PersonsService]
 })
--- Frontend/HumanitarianAid-UI/src/app/features/persons/persons.component.ts
+++ Frontend/HumanitarianAid-UI/src/app/features/persons/persons.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
 import { Person } from 'src/app/shared';
 
 import { PersonsService } from './services';
@@ -9,10 +11,21 @@ import { PersonsService } from './services';
   templateUrl: './persons.component.html',
   styleUrls: ['./persons.component.scss']
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   persons: Person[] = [];
   filteredPersons: Person[] = [];
-  displayedColumns: ['name', 'surname', 'age', 'gender']
+  dataSource = new MatTableDataSource<Person>(this.filteredPersons);
+  displayedColumns: string[] = ['name', 'surname', 'age', 'gender'];
+  displayedFilterColumns: string[] = ['name-filter', 'surname-filter', 'age-filter', 'gender-filter'];
+  genders: { value: string, label: string }[] = [{ value: null, label: '' }, { value: "Male", label: "Male" }, { value: "Female", label: "Female" }, { value: "Other", label: "Other" }]
+  filters: Person = {
+    name: '',
+    surname: '',
+    age: null,
+    gender: ''
+  }
 
   constructor(
     private readonly service: PersonsService,
@@ -22,29 +35,50 @@ export class PersonsComponent implements OnInit {
   ngOnInit(): void {
     this.getPersons();
   }
-  
-  // private buildFilterForm(): void {
-  //   this.filterForm = new FormGroup({
-  //     shelterName: new FormControl(''),
-  //     availablePlaces: new FormControl(''),
-  //   });
 
-  //   this.filterForm.controls['shelterName'].valueChanges.subscribe((value) => {
-  //     this.filterPersonssByPersonName(value);
-  //   });
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
 
-  //   this.filterForm.controls['availablePlaces'].valueChanges.subscribe(
-  //     (value) => {
-  //       this.filterSheltersByAvailablePlaces(value);
-  //     }
-  //   );
-  // }
+  filterchanged(value: string | number, field: string): void {
+    this.filters[field] = value;
+    this.applyFilter();
+  }
+
+  private applyFilter(): void {
+    this.filteredPersons = [...this.persons];
+    if (this.filters.age) {
+      this.filteredPersons = this.filteredPersons.filter((
+        person: Person
+      ) => person.age === Number(this.filters.age));
+    }
+    if (this.filters.gender) {
+      this.filteredPersons = this.filteredPersons.filter((
+        person: Person
+      ) => person.gender === this.filters.gender);
+    }
+    if (this.filters.name) {
+      this.filteredPersons = this.filteredPersons.filter((
+        person: Person
+      ) => person.name.includes(this.filters.name));
+    }
+    if (this.filters.surname) {
+      this.filteredPersons = this.filteredPersons.filter((
+        person: Person
+      ) => person.surname.includes(this.filters.surname));
+    }
+    console.log(this.filters, this.filteredPersons);
+    this.dataSource.filteredData = this.filteredPersons;
+    this.dataSource.data = [...this.filteredPersons];
+    console.log(this.dataSource.data, this.dataSource.filteredData)
+  }
 
   private getPersons(): void {
     this.service.getPersons().subscribe(
       (data: Person[]) => {
         this.persons = data;
         this.filteredPersons = this.persons;
+        this.dataSource.data = [...this.persons];
       },
       (error) => {
         this.snackBar.open(error.message, '', {
@@ -56,7 +90,7 @@ export class PersonsComponent implements OnInit {
       }
     );
   }
-  
+
   private filterPersonssByPersonName(personName: string): void {
     this.filteredPersons = this.persons.filter((person: Person) =>
       person.name.toLowerCase().includes(personName.toLowerCase())
